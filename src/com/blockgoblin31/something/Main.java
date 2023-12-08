140622fb46fa938e416187ce9c20341b7a8841f9
blockgoblin31 <57573828+tuc56@users.noreply.github.com>
2023-12-07 18:02:24 -0600

    Update Main.java
    Got rid of a for loop by not being dumb

--- src/com/blockgoblin31/something/Main.java
+++ src/com/blockgoblin31/something/Main.java
@@ -1,7 +1,5 @@
 package com.blockgoblin31.something;
 
-import com.google.gson.GsonBuilder;
-
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
@@ -26,33 +24,13 @@ public class Main {
         ArrayList<String> reversedLines = Iterator.loop(lines, new FunctionPasser<>() {
             @Override
             public String get(String input) {
-                ArrayList<Character> chars = Iterator.loop(PrimitiveFixer.charToCharacter(input.toCharArray()), new FunctionPasser<>() {
-
-                    final ArrayList<Character> output = new ArrayList<>();
-
-                    @Override
-                    public Character get(Character input) {
-                        output.add(0, input);
-                        return input;
-                    }
-
-                    @Override
-                    public ArrayList<Character> getFinal(ArrayList<Character> input) {
-                        return output;
-                    }
-
-                    @Override
-                    public Object get() {
-                        return null;
-                    }
-                });
 
                 FunctionPasser<Character> string = new FunctionPasser<>() {
                     final StringBuilder string = new StringBuilder();
 
                     @Override
                     public Character get(Character input) {
-                        string.append(input);
+                        string.insert(0, input);
                         return input;
                     }
 
@@ -66,7 +44,7 @@ public class Main {
                     }
                 };
 
-                Iterator.loop(chars, string);
+                Iterator.loop(PrimitiveFixer.charToCharacter(input.toCharArray()), string);
                 return (String) string.get();
             }
 
