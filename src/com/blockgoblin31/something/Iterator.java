4fc7f4d0a2474f46662c9432cbf62b8e88bef253
blockgoblin31 <57573828+tuc56@users.noreply.github.com>
2023-12-08 11:07:45 -0600

    Update Iterator.java
    Made it more cursed with recursive functions

--- src/com/blockgoblin31/something/Iterator.java
+++ src/com/blockgoblin31/something/Iterator.java
@@ -4,19 +4,28 @@ import java.util.ArrayList;
 import java.util.function.Supplier;
 
 public class Iterator {
-    public static <T> ArrayList<T> loop(Iterable<T> thing, FunctionPasser<T> func) {
+
+    public static <T> ArrayList<T> loop(ArrayList<T> thing, FunctionPasser<T> func) {
         ArrayList<T> output = new ArrayList<>();
-        for (T t : thing) {
-            output.add(func.get(t));
-        }
+        loop(thing, func, output, 0, thing.size());
         return func.getFinal(output);
+    }
+
+    public static <T> ArrayList<T> loop(ArrayList<T> thing, FunctionPasser<T> func, ArrayList<T> output, int curNum, int endNum) {
+        if (curNum >= endNum) return output;
+        output.add(func.get(thing.get(curNum)));
+        return loop(thing, func, output, curNum + 1, endNum);
     }
 
     public static <T> ArrayList<T> whileLoop(Supplier<Boolean> condition, FunctionPasser<T> func) {
         ArrayList<T> output = new ArrayList<>();
-        while (condition.get()) {
-            func.get();
-        }
+        whileLoop(condition, func, output);
         return func.getFinal(output);
+    }
+
+    static <T> ArrayList<T> whileLoop(Supplier<Boolean> condition, FunctionPasser<T> func, ArrayList<T> output) {
+        if (!condition.get()) return output;
+        func.get();
+        return whileLoop(condition, func, output);
     }
 }
