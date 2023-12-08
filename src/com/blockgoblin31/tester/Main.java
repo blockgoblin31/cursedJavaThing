9001c6afa9de1be59438adbabd469e2b84fc1646
blockgoblin31 <57573828+tuc56@users.noreply.github.com>
2023-12-08 12:02:21 -0600

    Updated tester class
    Added some comments and made the first method a literal step by step representation of what my complicated code is doing

--- src/com/blockgoblin31/tester/Main.java
+++ src/com/blockgoblin31/tester/Main.java
@@ -1,7 +1,9 @@
 package com.blockgoblin31.tester;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -16,21 +18,45 @@ public class Main {
         System.out.println(difference);
         System.out.println(secondDifference);
         System.out.println(secondDifference - difference);
+        time = System.currentTimeMillis();
+        readFileAndReverseLines("src\\text.txt");
+        System.out.println(System.currentTimeMillis() - time);
     }
 
+    //a direct copy of what my overcomplicated code is doing except in a single method
     static void readFile() throws FileNotFoundException {
-        ArrayList<String> output = new ArrayList<>();
+        ArrayList<String> input = new ArrayList<>();
         Scanner in = new Scanner(new File("src\\text.txt"));
         while (in.hasNext()) {
-            output.add(in.nextLine());
+            input.add(in.nextLine());
         }
-        for (String s : output) {
+        ArrayList<String> output = new ArrayList<>();
+        for (String s : input) {
             char[] chars = s.toCharArray();
             StringBuilder newString = new StringBuilder();
-            for (char c : chars) {
-                newString.insert(0, c);
+            for (int i = chars.length - 1; i > -1; i--) {
+                newString.append(chars[i]);
             }
-            System.out.println(newString.toString());
+            output.add(newString.toString());
+        }
+        StringBuilder toPrint = new StringBuilder();
+        for (String s : output) {
+            toPrint.append(s);
+            toPrint.append("\n");
+        }
+        toPrint.deleteCharAt(toPrint.length() - 1);
+        System.out.println(toPrint);
+    }
+
+    //good code by milesman13
+    public static void readFileAndReverseLines(String filePath) throws FileNotFoundException {
+        BufferedReader in = new BufferedReader(new FileReader(filePath));
+
+        for (String line : in.lines().toList()) {
+            StringBuffer buf = new StringBuffer(line);
+            buf.reverse();
+
+            System.out.println(buf);
         }
     }
 }
